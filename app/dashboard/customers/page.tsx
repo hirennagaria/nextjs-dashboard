26a6c508d55fb0cbe66382d0c95285b779542eaf
Hiren Nagaria <hirennagaria@yahoo.co.in>
2023-11-23 16:24:52 +0530

    next js dashboard

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,39 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { fetchCustomers, fetchCustomersPages } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import { lusitana } from "@/app/ui/fonts";
+import Pagination from "@/app/ui/invoices/pagination";
+import Search from "@/app/ui/search";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchCustomersPages(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+      </div>
+      <CustomersTable query={query} currentPage={currentPage} />
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
